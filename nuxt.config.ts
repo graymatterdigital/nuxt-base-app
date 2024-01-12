916fe0dc9e4e6c910bdf7ee08c224cf2f5fc8afa
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2024-01-12 00:55:42 -0500

    whammy
    coming together

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      "meta": [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        }
+      ]
+    }
+  },
+  modules: [
+    '@hebilicious/authjs-nuxt',
+    'nuxt-anchorscroll',
+    'nuxt-content-assets',
+    '@nuxt/content',
+    '@nuxt/image',
+    'nuxt-lodash',
+    '@nuxt/ui'
+  ],
+  googleSignIn: {
+    clientId: 'CLIENT ID OBTAINED FROM GOOGLE API CONSOLE',
+  },
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
+    },
+    github: {
+      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        verifyClientOnEveryRequest: true // whether to hit the /auth/session endpoint on every client request
+      }
+    }
+  }
 })
