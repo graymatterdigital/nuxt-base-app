4508b88f4837c743a0ad76f1120d88628e82df17
ub3rb3457 <31402226+ub3rb3457@users.noreply.github.com>
2024-01-17 14:00:05 -0500

    github login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content', "@nuxt/ui","@hebilicious/authjs-nuxt"],
+  /* authJs: {
+    verifyClientOnEveryRequest: false,
+    guestRedirectTo: "/about", // where to redirect if the user is not authenticated
+    authenticatedRedirectTo: "/", // where to redirect if the user is authenticated
+    baseUrl: "http://localhost:3000" // should be something like https://www.my-app.com
+  }, */
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
+    },
+    github: {
+      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        verifyClientOnEveryRequest: false, // whether to hit the /auth/session endpoint on every client request
+        guestRedirectTo: "/about", // where to redirect if the user is not authenticated
+        authenticatedRedirectTo: "/"
+      }
+    }
+  }
 })
